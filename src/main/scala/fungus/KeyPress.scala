ea2b2bca8a89055830176595311c5000ba044e3d
Guy John <guy@rumblesan.com>
2014-07-16 22:48:30 +0100

    Adding types for KeyPresses

---
+++ src/main/scala/fungus/KeyPress.scala
@@ -0,0 +1,11 @@
+package com.rumblesan.fungus
+
+sealed trait KeyPress
+
+case object UnknownKey extends KeyPress
+case object UpKey      extends KeyPress
+case object DownKey    extends KeyPress
+case object LeftKey    extends KeyPress
+case object RightKey   extends KeyPress
+
+
