ce80bfe265b7b65d190c3da53b1e528389656adf
Guy John <guy@rumblesan.com>
2014-07-20 20:03:33 +0100

    Adding LetterKey data type

--- src/main/scala/fungus/KeyPress.scala
+++ src/main/scala/fungus/KeyPress.scala
@@ -2,10 +2,10 @@ package com.rumblesan.fungus
 
 sealed trait KeyPress
 
-case object UnknownKey extends KeyPress
-case object UpKey      extends KeyPress
-case object DownKey    extends KeyPress
-case object LeftKey    extends KeyPress
-case object RightKey   extends KeyPress
-
+case object UnknownKey         extends KeyPress
+case object UpKey              extends KeyPress
+case object DownKey            extends KeyPress
+case object LeftKey            extends KeyPress
+case object RightKey           extends KeyPress
+case class  LetterKey(c: Char) extends KeyPress
 
