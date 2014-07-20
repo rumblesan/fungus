c6fc518aee82ef07b5838abc8a3998ecb26912a1
Guy John <guy@rumblesan.com>
2014-07-20 11:24:25 +0100

    Changing functions in DisplayFungus to use StateMonad

--- src/main/scala/fungus/display/DisplayFungus.scala
+++ src/main/scala/fungus/display/DisplayFungus.scala
@@ -1,41 +1,47 @@
 package com.rumblesan.fungus.display
 
+import scalaz._, Scalaz._
+import processing.core.{ PApplet, PConstants }
+
 import com.rumblesan.fungus.befunge.{ VM }
 import com.rumblesan.fungus.{ FungusMachine }
-import processing.core.{ PApplet, PConstants }
+import com.rumblesan.fungus.Types._
 
 object DisplayFungus {
 
-  def draw(p: PApplet, config: DrawingConfig)(f: FungusMachine): Unit = {
-
-    drawGrid(p, config)(f)
-
-    drawCursor(p, config)(f)
-
-  }
-
-  def drawGrid(p: PApplet, config: DrawingConfig)(f: FungusMachine): Unit = {
-
-    val vm = f.vm
-
-    p.fill(200, 200, 200)
+  def draw(p: PApplet, config: DrawingConfig): FungusState[Unit] = {
     for {
-      x <- (0 until vm.xSize)
-      y <- (0 until vm.ySize)
-      (xCoord, yCoord) = cellCoords(config)(x, y)
-      _ = p.rect(xCoord, yCoord, config.cellSize, config.cellSize)
+      _ <- drawGrid(p, config)
+      _ <- drawCursor(p, config)
     } yield Unit
-
   }
 
-  def drawCursor(p: PApplet, config: DrawingConfig)(f: FungusMachine): Unit = {
+  def drawGrid(p: PApplet, config: DrawingConfig): FungusState[Unit] = {
+    State.gets(f => {
 
-    val cursor = f.cursor
+      val vm = f.vm
+      p.fill(200, 200, 200)
+      for {
+        x <- (0 until vm.xSize)
+        y <- (0 until vm.ySize)
+        (xCoord, yCoord) = cellCoords(config)(x, y)
+        _ = p.rect(xCoord, yCoord, config.cellSize, config.cellSize)
+      } yield Unit
 
-    p.fill(255, 50, 50)
-    val (cursorX, cursorY) = cellCoords(config)(f.cursor.x, f.cursor.y)
-    p.rect(cursorX, cursorY, config.cellSize, config.cellSize)
+    })
+  }
 
+  def drawCursor(
+    p: PApplet, config: DrawingConfig
+  ): FungusState[Unit] = {
+    State.gets(f => {
+
+      val cursor = f.cursor
+      p.fill(255, 50, 50)
+      val (cursorX, cursorY) = cellCoords(config)(f.cursor.x, f.cursor.y)
+      p.rect(cursorX, cursorY, config.cellSize, config.cellSize)
+
+    })
   }
 
   def cellCoords(config: DrawingConfig)(xVal: Int, yVal: Int): (Int, Int) = {
