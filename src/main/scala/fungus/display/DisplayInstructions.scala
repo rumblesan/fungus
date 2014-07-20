d2d6fe325831e504ed045cfa1d41c1b09ee469a5
Guy John <guy@rumblesan.com>
2014-07-20 15:56:18 +0100

    Adding function for drawing instructions

---
+++ src/main/scala/fungus/display/DisplayInstructions.scala
@@ -0,0 +1,32 @@
+package com.rumblesan.fungus.display
+
+import scalaz._, Scalaz._
+import processing.core.{ PApplet, PConstants }
+
+import com.rumblesan.fungus.befunge.Types._
+import com.rumblesan.fungus.befunge._
+
+
+object DisplayInstructions {
+
+  def drawInstruction(
+    p: PApplet, config: DrawingConfig
+  )(
+    xPos: Int, yPos: Int
+  ): VMState[Unit] = {
+    Grid.getGridCell(xPos, yPos).map( i =>
+      i match {
+        case Up    => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+        case Down  => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+        case Left  => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+        case Right => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+        case NOP   => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+        case _     => p.rect(xPos, yPos, config.cellSize, config.cellSize)
+      }
+    )
+  }
+
+
+}
+
+
